15dfd636a0f8221c1d9bc6d89f91b5f7cb17ab22
noah <noah@supergene.co>
2023-08-04 16:23:38 +0900

    Declare service component Rename extension property of type of Long

--- data/src/main/java/wing/tree/bionda/data/extension/Long.kt
+++ data/src/main/java/wing/tree/bionda/data/extension/Long.kt
@@ -2,6 +2,6 @@ package wing.tree.bionda.data.extension
 
 val Long.Companion.fiveSecondsInMilliseconds: Long get() = 5000L
 val Long.Companion.negativeOne: Long get() = -1L
-val Long.Companion.oneSecond: Long get() = 1000L
+val Long.Companion.oneSecondInMilliseconds: Long get() = 1000L
 val Long.Companion.zero: Long get() = 0L
 val Long.int: Int get() = toInt()
