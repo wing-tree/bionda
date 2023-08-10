ce0825d792577bb334f8818c7f6f60272b1f4242
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-08-10 09:33:34 +0900

    Cleanup

--- data/src/main/java/wing/tree/bionda/data/extension/Int.kt
+++ data/src/main/java/wing/tree/bionda/data/extension/Int.kt
@@ -8,6 +8,7 @@ val Int.Companion.sixty: Int get() = 60
 val Int.Companion.ten: Int get() = 10
 val Int.Companion.three: Int get() = 3
 val Int.Companion.zero: Int get() = 0
+val Int.half: Int get() = div(2)
 
 fun Int.ifZero(defaultValue: () -> Int) = if (this `is` Int.zero) {
     defaultValue()
--- data/src/main/java/wing/tree/bionda/data/extension/Long.kt
+++ data/src/main/java/wing/tree/bionda/data/extension/Long.kt
@@ -6,5 +6,5 @@ val Long.Companion.negativeOne: Long get() = -1L
 val Long.Companion.oneHundred: Long get() = 100L
 val Long.Companion.oneSecondInMilliseconds: Long get() = 1000L
 val Long.Companion.zero: Long get() = 0L
-val Long.hundreds: Long get() = times(Long.oneHundred)
+val Long.hundreds: Long get() = times(100)
 val Long.int: Int get() = toInt()
