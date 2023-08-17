67cb8f91dfa9122749c148dcbe7dfa8aa226d245
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-08-18 00:50:48 +0900

    Add new string resources and extension functions.

--- data/src/main/java/wing/tree/bionda/data/extension/Long.kt
+++ data/src/main/java/wing/tree/bionda/data/extension/Long.kt
@@ -5,6 +5,7 @@ val Long.Companion.fiveSecondsInMilliseconds: Long get() = 5000L
 val Long.Companion.negativeOne: Long get() = -1L
 val Long.Companion.oneHundred: Long get() = 100L
 val Long.Companion.oneSecondInMilliseconds: Long get() = 1000L
+val Long.Companion.three: Long get() = 3L
 val Long.Companion.zero: Long get() = 0L
 val Long.hundreds: Long get() = times(100)
 val Long.int: Int get() = toInt()
