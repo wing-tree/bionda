26100a585735f791ed7a872cc34af7fcb75d5789
noah <noah@supergene.co>
2023-08-18 16:06:07 +0900

    Remove unnecessary annotation.

--- data/src/main/java/wing/tree/bionda/data/module/ServiceModule.kt
+++ data/src/main/java/wing/tree/bionda/data/module/ServiceModule.kt
@@ -12,7 +12,6 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object ServiceModule {
     @Provides
-    @Singleton
     fun providesForecastService(
         retrofit: Retrofit
     ): ForecastService {
