9654f92777c43df406f895c0631b4557b6227ea6
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-08-22 09:38:06 +0900

    Add new extension function.

--- data/src/main/java/wing/tree/bionda/data/extension/Int.kt
+++ data/src/main/java/wing/tree/bionda/data/extension/Int.kt
@@ -12,6 +12,7 @@ val Int.Companion.ten: Int get() = 10
 val Int.Companion.three: Int get() = 3
 val Int.Companion.two: Int get() = 2
 val Int.Companion.zero: Int get() = 0
+val Int.float: Float get() = toFloat()
 val Int.half: Int get() = div(2)
 val Int.long: Long get() = toLong()
 val Int.string: String get() = toString()
