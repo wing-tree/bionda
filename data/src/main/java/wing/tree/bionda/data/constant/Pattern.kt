da15bc55455fb2ba2a02e519cbbf03edcf3b50da
noah <noah@supergene.co>
2023-08-22 16:15:53 +0900

    Add new calendar pattern and format

--- data/src/main/java/wing/tree/bionda/data/constant/Pattern.kt
+++ data/src/main/java/wing/tree/bionda/data/constant/Pattern.kt
@@ -2,5 +2,6 @@ package wing.tree.bionda.data.constant
 
 const val PATTERN_BASE_TIME = "HHmm"
 const val PATTERN_BASE_DATE = "yyyyMMdd"
-const val PATTERN_TM_FC = "yyyyMMddHHmm"
+const val PATTERN_DAY_OF_MONTH = "M.dd"
 const val PATTERN_DT_FC = "yyyyMMdd"
+const val PATTERN_TM_FC = "yyyyMMddHHmm"
