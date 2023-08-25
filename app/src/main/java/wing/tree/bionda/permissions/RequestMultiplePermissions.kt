2cb30daef9994d32340e9da0401179807009284b
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-08-25 23:14:32 +0900

    Remove unnecessary CoroutineScope implementation

--- app/src/main/java/wing/tree/bionda/permissions/RequestMultiplePermissions.kt
+++ app/src/main/java/wing/tree/bionda/permissions/RequestMultiplePermissions.kt
@@ -3,19 +3,12 @@ package wing.tree.bionda.permissions
 import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
 import wing.tree.bionda.data.extension.one
-import wing.tree.bionda.data.extension.single
 import wing.tree.bionda.permissions.PermissionChecker.Result
 import wing.tree.bionda.permissions.PermissionChecker.State
 
-@OptIn(ExperimentalCoroutinesApi::class)
-class RequestMultiplePermissions : PermissionChecker,
-    CoroutineScope by CoroutineScope(Dispatchers.IO.limitedParallelism(Int.single))
-{
+class RequestMultiplePermissions : PermissionChecker {
     private val contract = ActivityResultContracts.RequestMultiplePermissions()
     private var activityResultLauncher: ActivityResultLauncher<Array<String>>? = null
     private var channel: Channel<Result>? = null
