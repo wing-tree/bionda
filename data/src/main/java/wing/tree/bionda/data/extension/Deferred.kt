a434992481f76be7c0d77ed698c9af898876e4ca
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-08-26 18:39:03 +0900

    Define Deferred extension functions

--- data/src/main/java/wing/tree/bionda/data/extension/Deferred.kt
+++ data/src/main/java/wing/tree/bionda/data/extension/Deferred.kt
@@ -1,9 +1,15 @@
 package wing.tree.bionda.data.extension
 
 import kotlinx.coroutines.Deferred
+import wing.tree.bionda.data.model.State.Complete
 
 suspend fun <T> Deferred<T>.awaitOrElse(defaultValue: (Exception) -> T) = try {
     await()
 } catch (exception: Exception) {
     defaultValue(exception)
 }
+
+suspend fun <T> Deferred<Complete<T>>.awaitOrFailure(): Complete<T> =
+    awaitOrElse {
+        Complete.Failure(it)
+    }
