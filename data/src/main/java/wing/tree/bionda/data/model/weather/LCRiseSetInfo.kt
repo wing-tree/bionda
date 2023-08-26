dbad154d89dba81c265d9c19f1b9b0a9efd9f555
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-08-26 23:22:58 +0900

    Apply xml converter

--- data/src/main/java/wing/tree/bionda/data/model/weather/LCRiseSetInfo.kt
+++ data/src/main/java/wing/tree/bionda/data/model/weather/LCRiseSetInfo.kt
@@ -2,37 +2,59 @@ package wing.tree.bionda.data.model.weather
 
 import androidx.room.Entity
 import androidx.room.Ignore
+import com.tickaroo.tikxml.annotation.Element
+import com.tickaroo.tikxml.annotation.PropertyElement
+import com.tickaroo.tikxml.annotation.Xml
 import kotlinx.serialization.Serializable
 import wing.tree.bionda.data.constant.COMMA
 import wing.tree.bionda.data.constant.SPACE
 import wing.tree.bionda.data.exception.OpenApiError
 import wing.tree.bionda.data.exception.second
 import wing.tree.bionda.data.exception.third
-import wing.tree.bionda.data.validator.ResponseValidator
+import wing.tree.bionda.data.extension.not
 
 sealed interface LCRiseSetInfo {
     val item: Item
 
     @Serializable
+    @Xml
     data class Item(
+        @PropertyElement
         val locdate: String,
+        @PropertyElement
         val location: String,
+        @PropertyElement
         val longitude: Int,
+        @PropertyElement
         val longitudeNum: Double,
+        @PropertyElement
         val latitude: Int,
+        @PropertyElement
         val latitudeNum: Double,
+        @PropertyElement
         val sunrise: String,
+        @PropertyElement
         val suntransit: String,
+        @PropertyElement
         val sunset: String,
+        @PropertyElement
         val moonrise: String,
+        @PropertyElement
         val moontransit: String,
+        @PropertyElement
         val moonset: String,
+        @PropertyElement
         val civilm: String,
+        @PropertyElement
         val civile: String,
+        @PropertyElement
         val nautm: String,
+        @PropertyElement
         val naute: String,
+        @PropertyElement
         val astm: String,
-        val aste: String,
+        @PropertyElement
+        val aste: String
     )
 
     @Entity("lc_rise_set_info", primaryKeys = ["locdate", "longitude", "latitude"])
@@ -50,11 +72,11 @@ sealed interface LCRiseSetInfo {
     }
 
     data class Remote(
-        override val response: Response<Item>
-    ) : LCRiseSetInfo, ResponseValidator {
+        val response: Response
+    ) : LCRiseSetInfo {
         override val item: Item = response.items.first()
 
-        override fun validate(vararg params: String) {
+        private fun validate(vararg params: String) {
             if (response.isUnsuccessful) {
                 val header = response.header
                 val errorCode = header.resultCode
@@ -84,4 +106,41 @@ sealed interface LCRiseSetInfo {
             )
         }
     }
+
+    @Xml
+    data class Response(
+        @Element
+        val header: Header,
+        @Element
+        val body: Body
+    ) {
+        val isUnsuccessful: Boolean get() = header.resultCode not OpenApiError.ERROR_CODE_00
+        val items: Items get() = body.items
+    }
+
+    @Xml
+    data class Header(
+        @PropertyElement
+        val resultCode: String,
+        @PropertyElement
+        val resultMsg: String
+    )
+
+    @Xml
+    data class Body(
+        @Element
+        val items: Items,
+        @PropertyElement
+        val numOfRows: Int,
+        @PropertyElement
+        val pageNo: Int,
+        @PropertyElement
+        val totalCount: Int
+    )
+
+    @Xml
+    data class Items(
+        @Element
+        val item: List<Item> = emptyList()
+    ) : List<Item> by item
 }
