f147ddbc2a0e25f66e33a4766a93c9933639de82
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-08-26 18:40:06 +0900

    Add time advance extension function to string

--- data/src/main/java/wing/tree/bionda/data/extension/String.kt
+++ data/src/main/java/wing/tree/bionda/data/extension/String.kt
@@ -1,10 +1,24 @@
 package wing.tree.bionda.data.extension
 
+import android.icu.text.SimpleDateFormat
+import wing.tree.bionda.data.top.level.koreaCalendar
+import java.util.Locale
+
 val String.Companion.degree: String get() = "°"
 val String.Companion.empty: String get() = ""
 val String.Companion.zero: String get() = "0"
 val String.floatOrNull: Float? get() = toFloatOrNull()
 val String.int: Int get() = toInt()
+
+fun String.advanceHourOfDayBy(hourOfDay: Int, pattern: String): String {
+    val simpleDateFormat = SimpleDateFormat(pattern, Locale.KOREA)
+
+    return koreaCalendar(simpleDateFormat.parse(this)).let {
+        it.hourOfDay -= hourOfDay
+
+        simpleDateFormat.format(it)
+    }
+}
 
 fun String.ifZero(defaultValue: () -> String) = if (this `is` String.zero) {
     defaultValue()
