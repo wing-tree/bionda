c9e2239f697a0a97814c7b32e89ff3f5ad31c9e8
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-08-28 00:03:10 +0900

    Add Area model and dao

---
+++ data/src/main/java/wing/tree/bionda/data/database/dao/AreaDao.kt
@@ -0,0 +1,26 @@
+package wing.tree.bionda.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import wing.tree.bionda.data.model.Area
+
+@Dao
+interface AreaDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(area: Area)
+
+    @Suppress("unused")
+    @Update
+    suspend fun update(area: Area)
+
+    @Suppress("unused")
+    @Delete
+    suspend fun delete(area: Area)
+
+    @Query("SELECT * FROM area")
+    suspend fun load(): List<Area>
+}
