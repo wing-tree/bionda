bf5f448b79731b687280242cacba4b81dfb54aed
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-08-27 14:15:32 +0900

    Cleanup

--- data/src/main/java/wing/tree/bionda/data/model/DegreeMinute.kt
+++ data/src/main/java/wing/tree/bionda/data/model/DegreeMinute.kt
@@ -1,13 +1,9 @@
 package wing.tree.bionda.data.model
 
-import wing.tree.bionda.data.extension.int
-
 data class DegreeMinute(
     val degree: Int,
     val minute: Int
 ) {
-    val int: Int get() = toString().int
-
     override fun toString(): String {
         return "$degree${String.format("%02d", minute)}"
     }
