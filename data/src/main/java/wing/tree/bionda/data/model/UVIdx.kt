613f1587fc6f256d619eee193ce7a3d4c4897e7b
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-08-28 12:39:09 +0900

    Implement UVIdx model on progress..

--- data/src/main/java/wing/tree/bionda/data/model/UVIdx.kt
+++ data/src/main/java/wing/tree/bionda/data/model/UVIdx.kt
@@ -1,10 +1,13 @@
 package wing.tree.bionda.data.model
 
+import androidx.room.Entity
 import kotlinx.serialization.Serializable
 import wing.tree.bionda.data.model.core.Response
 import wing.tree.bionda.data.validator.ResponseValidator
 
 sealed interface UVIdx {
+    val item: Item
+
     @Serializable
     data class Item(
         val code: String,
@@ -38,10 +41,19 @@ sealed interface UVIdx {
         val h75: Int
     )
 
+    @Entity(tableName = "uv_idx", primaryKeys = ["areaNo", "time"])
+    data class Local(
+        override val item: Item,
+        val areaNo: String,
+        val time: String
+    ) : UVIdx
+
     @Serializable
     data class Remote(
         override val response: Response<Item>
     ) : UVIdx, ResponseValidator {
+        override val item: Item get() = response.items.first()
+
         override fun validate(vararg params: String) {
             TODO("Not yet implemented")
         }
