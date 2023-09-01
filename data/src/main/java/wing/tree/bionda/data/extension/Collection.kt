6ba84fad1ceccbb2bdb3dde9fc0fce445f5d5626
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-09-02 01:00:37 +0900

    Add Collection extension properties and functions

--- data/src/main/java/wing/tree/bionda/data/extension/Collection.kt
+++ data/src/main/java/wing/tree/bionda/data/extension/Collection.kt
@@ -1,7 +1,20 @@
 package wing.tree.bionda.data.extension
 
+import wing.tree.bionda.data.core.State
+
+val Collection<State.Complete.Failure>.exceptions: List<Throwable> get() = map {
+    it.throwable
+}
+
+val <T> Collection<State.Complete.Success<T>>.values: List<T> get() = map {
+    it.value
+}
+
 fun <T> Collection<T>.containsAny(elements: Collection<T>): Boolean {
     return elements.any {
         contains(it)
     }
 }
+
+fun <T> Collection<State.Complete<T>>.failed() = filterIsInstance<State.Complete.Failure>()
+fun <T> Collection<State.Complete<T>>.succeeded() = filterIsInstance<State.Complete.Success<T>>()
