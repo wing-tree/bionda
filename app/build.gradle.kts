ea1ae909565afca3e2028f3820900e46e44b6655
noah <noah@supergene.co>
2023-09-08 16:10:48 +0900

    Connect to firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.android.application")
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
+    id("com.google.gms.google-services")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.kapt")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     id("com.android.library") version "8.1.1" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
     id("com.google.devtools.ksp") version "1.8.21-1.0.11" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
     id("org.jetbrains.kotlin.android") version "1.8.21" apply false
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.serialization") version "1.8.21"
