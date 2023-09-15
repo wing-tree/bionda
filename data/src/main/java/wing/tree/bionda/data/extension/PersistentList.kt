0a833cb4f4e80c87050fad655af5e7db97bf1000
noah <noah@supergene.co>
2023-09-15 18:33:00 +0900

    Add Timber and cleanup

--- data/src/main/java/wing/tree/bionda/data/extension/PersistentList.kt
+++ data/src/main/java/wing/tree/bionda/data/extension/PersistentList.kt
@@ -1,6 +1,7 @@
 package wing.tree.bionda.data.extension
 
 import kotlinx.collections.immutable.PersistentList
+import timber.log.Timber
 
 object Builder {
     fun <T> PersistentList.Builder<T>.replaceAt(index: Int, element: T): Boolean {
@@ -8,7 +9,8 @@ object Builder {
             removeAt(index)
             add(index, element)
             true
-        } catch (e: IndexOutOfBoundsException) {
+        } catch (t: IndexOutOfBoundsException) {
+            Timber.e(t)
             false
         }
     }
