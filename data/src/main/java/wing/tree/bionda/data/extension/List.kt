f1d9b8f4987f174eea7c35afd70487824feb12c9
noah <noah@supergene.co>
2023-09-15 19:19:03 +0900

    Rename function

--- data/src/main/java/wing/tree/bionda/data/extension/List.kt
+++ data/src/main/java/wing/tree/bionda/data/extension/List.kt
@@ -1,3 +1,3 @@
 package wing.tree.bionda.data.extension
 
-fun <T> List<T>.updatedBy(function: MutableList<T>.() -> Unit): List<T> = toMutableList().apply(function)
+fun <T> List<T>.updatedWith(function: MutableList<T>.() -> Unit): List<T> = toMutableList().apply(function)
