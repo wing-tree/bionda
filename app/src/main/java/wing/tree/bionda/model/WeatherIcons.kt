0c119a5ccebfce192a0c792db40e2985fbf88d42
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-09-16 10:55:49 +0900

    Make WeatherIcons Map properties ImmutableMap

--- app/src/main/java/wing/tree/bionda/model/WeatherIcons.kt
+++ app/src/main/java/wing/tree/bionda/model/WeatherIcons.kt
@@ -1,25 +1,27 @@
 package wing.tree.bionda.model
 
+import kotlinx.collections.immutable.ImmutableMap
+import kotlinx.collections.immutable.persistentMapOf
 import wing.tree.bionda.R
 
 private typealias DrawableRes = Int
 
 sealed interface WeatherIcons {
-    val sky: Map<String, DrawableRes>
-    val pty: Map<String, DrawableRes?>
-    val wf: Map<String, DrawableRes>
+    val sky: ImmutableMap<String, DrawableRes>
+    val pty: ImmutableMap<String, DrawableRes?>
+    val wf: ImmutableMap<String, DrawableRes>
 
     val sunrise: Int get() = R.drawable.wi_sunrise
     val sunset: Int get() = R.drawable.wi_sunset
 
     object Daytime : WeatherIcons {
-        override val sky: Map<String, DrawableRes> = mapOf(
+        override val sky: ImmutableMap<String, DrawableRes> = persistentMapOf(
             "1" to R.drawable.wi_day_sunny,
             "3" to R.drawable.wi_day_cloudy,
             "4" to R.drawable.wi_cloudy
         )
 
-        override val pty: Map<String, DrawableRes?> = mapOf(
+        override val pty: ImmutableMap<String, DrawableRes?> = persistentMapOf(
             "0" to null,
             "1" to R.drawable.wi_day_rain,
             "2" to R.drawable.wi_day_rain_mix,
@@ -27,7 +29,7 @@ sealed interface WeatherIcons {
             "4" to R.drawable.wi_day_showers
         )
 
-        override val wf: Map<String, DrawableRes> = mapOf(
+        override val wf: ImmutableMap<String, DrawableRes> = persistentMapOf(
             "맑음" to R.drawable.wi_day_sunny,
             "구름많음" to R.drawable.wi_day_cloudy,
             "구름많고 비" to R.drawable.wi_day_rain,
@@ -43,13 +45,13 @@ sealed interface WeatherIcons {
     }
 
     object Nighttime : WeatherIcons {
-        override val sky: Map<String, DrawableRes> = mapOf(
+        override val sky: ImmutableMap<String, DrawableRes> = persistentMapOf(
             "1" to R.drawable.wi_night_clear,
             "3" to R.drawable.wi_night_alt_cloudy,
             "4" to R.drawable.wi_cloudy
         )
 
-        override val pty: Map<String, DrawableRes?> = mapOf(
+        override val pty: ImmutableMap<String, DrawableRes?> = persistentMapOf(
             "0" to null,
             "1" to R.drawable.wi_night_alt_rain,
             "2" to R.drawable.wi_night_alt_rain_mix,
@@ -57,7 +59,7 @@ sealed interface WeatherIcons {
             "4" to R.drawable.wi_night_alt_showers
         )
 
-        override val wf: Map<String, DrawableRes> = mapOf(
+        override val wf: ImmutableMap<String, DrawableRes> = persistentMapOf(
             "맑음" to R.drawable.wi_night_clear,
             "구름많음" to R.drawable.wi_night_alt_cloudy,
             "구름많고 비" to R.drawable.wi_night_alt_rain,
