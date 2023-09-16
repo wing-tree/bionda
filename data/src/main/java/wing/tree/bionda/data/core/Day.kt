4104c6202d404d6c664b1fa2cb6d1a18c1b992e7
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-09-16 10:33:49 +0900

    Define Day sealed class

---
+++ data/src/main/java/wing/tree/bionda/data/core/Day.kt
@@ -0,0 +1,25 @@
+package wing.tree.bionda.data.core
+
+import wing.tree.bionda.data.extension.baseDate
+import wing.tree.bionda.data.extension.dayAfterTomorrow
+import wing.tree.bionda.data.extension.tomorrow
+import wing.tree.bionda.data.top.level.koreaCalendar
+
+sealed class Day {
+    abstract val baseDate: String
+
+    object Today : Day() {
+        override val baseDate: String
+            get() = koreaCalendar.baseDate
+    }
+
+    object Tomorrow : Day() {
+        override val baseDate: String
+            get() = koreaCalendar.tomorrow.baseDate
+    }
+
+    object DayAfterTomorrow : Day() {
+        override val baseDate: String
+            get() = koreaCalendar.dayAfterTomorrow.baseDate
+    }
+}
