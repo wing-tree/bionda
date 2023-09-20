3affad99c4f752450bca653f1b9d0291bd1687ff
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-09-21 08:33:55 +0900

    Add mostCommon extension function

--- data/src/main/java/wing/tree/bionda/data/extension/List.kt
+++ data/src/main/java/wing/tree/bionda/data/extension/List.kt
@@ -1,3 +1,15 @@
 package wing.tree.bionda.data.extension
 
+fun <T> List<T>.mostCommon(vararg excluded: T): T? {
+    return filterNot {
+        it in excluded
+    }.groupingBy {
+        it
+    }
+        .eachCount()
+        .maxByOrNull {
+            it.value
+        }?.key
+}
+
 fun <T> List<T>.updatedWith(function: MutableList<T>.() -> Unit): List<T> = toMutableList().apply(function)
