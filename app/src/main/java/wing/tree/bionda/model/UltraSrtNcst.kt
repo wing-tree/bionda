852971d7329c3feffec8b15374ff61a92945ac8d
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-09-27 21:02:56 +0900

    Introduce new properties

--- app/src/main/java/wing/tree/bionda/model/UltraSrtNcst.kt
+++ app/src/main/java/wing/tree/bionda/model/UltraSrtNcst.kt
@@ -1,16 +1,54 @@
 package wing.tree.bionda.model
 
 import kotlinx.collections.immutable.ImmutableMap
+import wing.tree.bionda.data.core.Season
+import wing.tree.bionda.data.core.season
 import wing.tree.bionda.data.extension.int
+import wing.tree.bionda.data.extension.isNotNull
+import wing.tree.bionda.data.extension.roundToOneDecimalPlace
 import wing.tree.bionda.data.extension.string
+import wing.tree.bionda.data.extension.zero
 import wing.tree.bionda.data.model.Category
 import wing.tree.bionda.data.model.CodeValue
+import wing.tree.bionda.top.level.calculateHeatIndex
+import wing.tree.bionda.top.level.calculateWindChill
 
 data class UltraSrtNcst(
     val baseDate: String,
     val baseTime: String,
-    val codeValues: ImmutableMap<String, Double>
+    val codeValues: ImmutableMap<String, Double>,
+    val tmn: String? = null,
+    val tmx: String? = null
 ) {
+    private val heatIndex: Double? get() = with(t1h) {
+        if (isNotNull()) {
+            calculateHeatIndex(
+                ta = this,
+                rh = reh ?: Double.zero
+            )
+                .roundToOneDecimalPlace()
+        } else {
+            null
+        }
+    }
+
+    private val windChill: Double? get() = with(t1h) {
+        if (isNotNull()) {
+            calculateWindChill(
+                ta = this,
+                v = wsd ?: Double.zero
+            )
+                .roundToOneDecimalPlace()
+        } else {
+            null
+        }
+    }
+
+    val feelsLikeTemperature: Double? get() = when(season) {
+        Season.SUMMER -> heatIndex
+        Season.WINTER -> windChill
+    }
+
     val t1h = codeValues[Category.T1H]
     val rn1 = codeValues[Category.RN1]
     val uuu = codeValues[Category.UUU]
