936aca245fe0dff2bafb56ca0f556b4021220fb7
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-09-28 08:58:00 +0900

    Apply night-mode

--- app/src/main/java/wing/tree/bionda/theme/Color.kt
+++ app/src/main/java/wing/tree/bionda/theme/Color.kt
@@ -10,6 +10,8 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val LightSkyBlue = Color(0xFF87CEFA)
+
 val YellowOrange = Color(0xFFFFAE42)
 
 val LightGray = Color(0xFFE5E5E5)
--- app/src/main/java/wing/tree/bionda/theme/Theme.kt
+++ app/src/main/java/wing/tree/bionda/theme/Theme.kt
@@ -1,30 +1,60 @@
 package wing.tree.bionda.theme
 
+import android.app.Activity
+import android.view.View
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
-    secondary = PurpleGrey40,
+    secondary = LightSkyBlue,
     tertiary = Pink40
+)
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple40,
+    secondary = LightSkyBlue,
+    tertiary = Pink40
 )
 
 @Composable
-fun BiondaTheme(content: @Composable () -> Unit) {
+fun BiondaTheme(
+    content: @Composable () -> Unit
+) {
+    val isSystemInDarkTheme = isSystemInDarkTheme()
+    val colorScheme = when {
+        isSystemInDarkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+
+    if (view.isNotInEditMode) {
+        SideEffect {
+            val context = view.context
+
+            if (context is Activity) {
+                val insetsController = WindowCompat.getInsetsController(
+                    context.window,
+                    view
+                )
+
+                insetsController.isAppearanceLightStatusBars = isSystemInDarkTheme.not()
+            }
+        }
+    }
+
     MaterialTheme(
-        colorScheme = LightColorScheme,
+        colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
 }
+
+private val View.isNotInEditMode: Boolean get() = isInEditMode.not()
