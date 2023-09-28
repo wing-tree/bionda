0c429be10de757c1224dd6eef318c38228fb75f2
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-09-28 09:03:59 +0900

    Improve component placement

--- app/src/main/java/wing/tree/bionda/view/MainActivity.kt
+++ app/src/main/java/wing/tree/bionda/view/MainActivity.kt
@@ -26,13 +26,13 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -128,11 +128,21 @@ class MainActivity : AppCompatActivity(), PermissionChecker {
                         CenterAlignedTopAppBar(
                             title = noOperations,
                             navigationIcon = {
-                                Icon(
-                                    imageVector = Icons.Default.Menu,
-                                    contentDescription = null,
-                                    modifier = Modifier.padding(horizontal = 16.dp)
+                                SingleChoiceSegmentedButtonRow(
+                                    selectedSegmentedButtonIndex = selectedSegmentedButtonIndex,
+                                    onClick = {
+                                        selectedSegmentedButtonIndex = it
+                                    },
+                                    modifier = Modifier.padding(windowSizeClass.marginValues)
                                 )
+                            },
+                            actions = {
+                                IconButton(onClick = { /*TODO*/ }) {
+                                    Icon(
+                                        imageVector = Icons.Default.Menu,
+                                        contentDescription = null
+                                    )
+                                }
                             }
                         )
                     }
@@ -142,14 +152,6 @@ class MainActivity : AppCompatActivity(), PermissionChecker {
                             .fillMaxSize()
                             .padding(innerPadding)
                     ) {
-                        SingleChoiceSegmentedButtonRow(
-                            selectedSegmentedButtonIndex = selectedSegmentedButtonIndex,
-                            onClick = {
-                                selectedSegmentedButtonIndex = it
-                            },
-                            modifier = Modifier.padding(windowSizeClass.marginValues)
-                        )
-
                         AnimatedContent(
                             targetState = selectedSegmentedButtonIndex,
                             modifier = Modifier.weight(Float.full),
