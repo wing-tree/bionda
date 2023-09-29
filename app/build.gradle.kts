230bdcfb604447aa945a089214bd29b2b921fe41
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-09-29 18:05:03 +0900

    Update dependencies version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,17 +86,18 @@ android {
 dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.02"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.compose.ui:ui:1.5.1")
-    implementation("androidx.compose.ui:ui-graphics:1.5.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.1")
-    implementation("androidx.compose.material3:material3-android:1.2.0-alpha07")
+    implementation("androidx.compose.material:material-android:1.5.2")
+    implementation("androidx.compose.material3:material3-android:1.2.0-alpha08")
+    implementation("androidx.compose.ui:ui:1.5.2")
+    implementation("androidx.compose.ui:ui-graphics:1.5.2")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.2")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
