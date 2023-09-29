30e146b60b8691267e6e264ab177a08893c01bcc
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-09-29 11:57:43 +0900

    Revert "Remove night theme"
    This reverts commit 1f826de8a72068756a651ef7b1a84691507ac2db.

--- app/src/main/java/wing/tree/bionda/theme/Theme.kt
+++ app/src/main/java/wing/tree/bionda/theme/Theme.kt
@@ -2,27 +2,37 @@ package wing.tree.bionda.theme
 
 import android.app.Activity
 import android.view.View
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = LightSkyBlue,
-    tertiary = Pink40,
-    background = Background,
-    onBackground = Color.Black,
-    surface = Surface
+    tertiary = Pink40
+)
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple40,
+    secondary = LightSkyBlue,
+    tertiary = Pink40
 )
 
 @Composable
 fun BiondaTheme(
     content: @Composable () -> Unit
 ) {
+    val isSystemInDarkTheme = isSystemInDarkTheme()
+    val colorScheme = when {
+        isSystemInDarkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
     val view = LocalView.current
 
     if (view.isNotInEditMode) {
@@ -35,13 +45,13 @@ fun BiondaTheme(
                     view
                 )
 
-                insetsController.isAppearanceLightStatusBars = true
+                insetsController.isAppearanceLightStatusBars = isSystemInDarkTheme.not()
             }
         }
     }
 
     MaterialTheme(
-        colorScheme = LightColorScheme,
+        colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
