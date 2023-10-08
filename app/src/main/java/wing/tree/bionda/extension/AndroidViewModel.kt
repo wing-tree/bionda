a607b719299165225f112e9b70accebfc6402551
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-10-08 10:11:54 +0900

    Modify extension function

--- app/src/main/java/wing/tree/bionda/extension/AndroidViewModel.kt
+++ app/src/main/java/wing/tree/bionda/extension/AndroidViewModel.kt
@@ -3,7 +3,7 @@ package wing.tree.bionda.extension
 import android.app.Application
 import android.content.pm.PackageManager
 import androidx.lifecycle.AndroidViewModel
+import wing.tree.bionda.data.extension.`is`
 
-fun AndroidViewModel.checkSelfPermission(vararg permission: String) = permission.all {
-    getApplication<Application>().checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED
-}
+fun AndroidViewModel.checkSelfPermission(permission: String) = getApplication<Application>()
+    .checkSelfPermission(permission) `is` PackageManager.PERMISSION_GRANTED
