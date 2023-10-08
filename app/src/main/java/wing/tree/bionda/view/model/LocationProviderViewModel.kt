6b25b8ccf6a00e44fa87c1af90c9686a3c51454e
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-10-08 10:13:13 +0900

    Add new property area and define it's setter

--- app/src/main/java/wing/tree/bionda/view/model/LocationProviderViewModel.kt
+++ app/src/main/java/wing/tree/bionda/view/model/LocationProviderViewModel.kt
@@ -7,8 +7,11 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import wing.tree.bionda.data.core.State
+import wing.tree.bionda.data.core.State.Complete
 import wing.tree.bionda.data.core.map
 import wing.tree.bionda.data.extension.ifTrue
+import wing.tree.bionda.data.extension.isNotNull
+import wing.tree.bionda.data.model.Area
 import wing.tree.bionda.data.provider.LocationProvider
 import wing.tree.bionda.extension.checkSelfPermission
 import wing.tree.bionda.extension.toCoordinate
@@ -23,11 +26,34 @@ abstract class LocationProviderViewModel(
         it.map(Location::toCoordinate)
     }
 
+    var area: Area? = null
+        set(value) {
+            if (value.isNotNull()) {
+                field = value
+
+                location.value = Location(null)
+                    .apply {
+                        latitude = value.latitude
+                        longitude = value.longitude
+                    }.let {
+                        Complete.Success(it)
+                    }
+            }
+        }
+
     fun load() {
-        locationPermissions.any {
-            checkSelfPermission(it)
-        }.ifTrue {
-            viewModelScope.launch {
+        viewModelScope.launch {
+            area?.let {
+                location.value = Location(null)
+                    .apply {
+                        latitude = it.latitude
+                        longitude = it.longitude
+                    }.let {
+                        Complete.Success(it)
+                    }
+            } ?: locationPermissions.any {
+                checkSelfPermission(it)
+            }.ifTrue {
                 location.value = locationProvider.getLocation().map {
                     it ?: LocationProvider.seoul
                 }
