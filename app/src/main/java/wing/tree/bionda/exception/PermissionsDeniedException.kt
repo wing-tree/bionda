c7dfc1c89e6b36ba9aa663f8d5044076e862dce9
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-10-08 11:03:58 +0900

    Declare secondary constructor

--- app/src/main/java/wing/tree/bionda/exception/PermissionsDeniedException.kt
+++ app/src/main/java/wing/tree/bionda/exception/PermissionsDeniedException.kt
@@ -1,5 +1,8 @@
 package wing.tree.bionda.exception
 
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
-data class PermissionsDeniedException(val permissions: ImmutableList<String>) : Exception()
+data class PermissionsDeniedException(val permissions: ImmutableList<String>) : Exception() {
+    constructor(permission: String) : this(persistentListOf(permission))
+}
