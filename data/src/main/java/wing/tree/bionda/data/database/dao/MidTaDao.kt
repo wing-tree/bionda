8a1db7c2cd7b63a1212fe9f3cc4668d10430e3ac
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-10-14 22:40:05 +0900

    Modify delete logic

--- data/src/main/java/wing/tree/bionda/data/database/dao/MidTaDao.kt
+++ data/src/main/java/wing/tree/bionda/data/database/dao/MidTaDao.kt
@@ -4,9 +4,11 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Transaction
+import wing.tree.bionda.data.extension.advanceHourOfDayBy
 import wing.tree.bionda.data.extension.tmFc
 import wing.tree.bionda.data.extension.yesterday
 import wing.tree.bionda.data.top.level.tmFcCalendar
+import java.time.LocalTime
 import wing.tree.bionda.data.model.MidTa.Local as MidTa
 
 @Dao
@@ -22,7 +24,7 @@ interface MidTaDao {
 
     @Transaction
     suspend fun cacheInTransaction(midTa: MidTa) {
-        deleteUpTo(tmFcCalendar.yesterday.tmFc)
+        deleteUpTo(tmFcCalendar.advanceHourOfDayBy(LocalTime.NOON.hour).tmFc)
         insert(midTa)
     }
 }
