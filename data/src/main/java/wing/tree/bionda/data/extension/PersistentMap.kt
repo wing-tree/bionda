402d8260913b766c08eea7589560ff6d516d7139
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-10-26 21:45:03 +0900

    Improve extension function

--- data/src/main/java/wing/tree/bionda/data/extension/PersistentMap.kt
+++ data/src/main/java/wing/tree/bionda/data/extension/PersistentMap.kt
@@ -2,10 +2,10 @@ package wing.tree.bionda.data.extension
 
 import kotlinx.collections.immutable.PersistentMap
 
-fun <K, V> PersistentMap<K, V>.putAllIfAbsent(other: PersistentMap<K, V>): PersistentMap<K, V> {
+fun <K, V> PersistentMap<K, V>.putAllIfAbsent(m: Map<K, V>): PersistentMap<K, V> {
     val builder = builder()
 
-    for ((key, value) in other) {
+    for ((key, value) in m) {
         builder.putIfAbsent(key, value)
     }
 
